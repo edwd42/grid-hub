36c866ab4b219d5665fe6733dc429d98744eb74f
edwd42 <edwd42@gmail.com>
2019-12-04 18:12:45 -0500

    add component layouts/default

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,13 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { DashboardComponent } from './modules/dashboard/dashboard.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent,
-    DashboardComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
--- src/app/layouts/default/default.module.ts
+++ src/app/layouts/default/default.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { DefaultComponent } from './default.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [DefaultComponent],
   imports: [
     CommonModule
   ]
