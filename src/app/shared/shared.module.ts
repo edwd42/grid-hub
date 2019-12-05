2c2aa2029b37554011ecd90850766ccc849c37c7
edwd42 <edwd42@gmail.com>
2019-12-04 19:05:08 -0500

    import MatDividerModule into SharedModule and add <mat-divider></mat-divider> to footer.component.html and add css

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,5 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { MatDividerModule } from '@angular/material';
 import { FooterComponent } from './components/footer/footer.component';
 import { HeaderComponent } from './components/header/header.component';
 import { SidebarComponent } from './components/sidebar/sidebar.component';
@@ -11,7 +12,8 @@ import { SidebarComponent } from './components/sidebar/sidebar.component';
     HeaderComponent, FooterComponent, SidebarComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    MatDividerModule
   ],
   exports: [
     HeaderComponent, FooterComponent, SidebarComponent
