e5e0e3c426fb93685e01d3098a79b48603a4e2b2
edwd42 <edwd42@gmail.com>
2019-12-07 10:31:10 -0500

    add chart exporting widget HC_exporting to AreaComponent

--- src/app/shared/widgets/area/area.component.ts
+++ src/app/shared/widgets/area/area.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import * as Highcharts from 'highcharts';
+import HC_exporting from 'highcharts/modules/exporting';
 
 @Component({
   selector: 'app-widget-area',
@@ -73,6 +74,13 @@ export class AreaComponent implements OnInit {
         data: [2, 2, 2, 6, 13, 30, 46]
       }]
     };
+    HC_exporting(Highcharts);
+
+    setTimeout(() => {
+      window.dispatchEvent(
+        new Event('resize')
+      );
+    }, 300);
 
   }
 
