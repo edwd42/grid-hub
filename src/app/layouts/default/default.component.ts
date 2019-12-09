1f4a8bf030c4e1bc6beeb33d0969d8224c9818af
edwd42 <edwd42@gmail.com>
2019-12-08 22:19:41 -0500

    remove map.module for leaflet

--- src/app/layouts/default/default.component.ts
+++ src/app/layouts/default/default.component.ts
@@ -1,19 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-default',
-  templateUrl: './default.component.html',
-  styleUrls: ['./default.component.scss']
+  selector: "app-default",
+  templateUrl: "./default.component.html",
+  styleUrls: ["./default.component.scss"]
 })
 export class DefaultComponent implements OnInit {
-
   sideBarOpen = true;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() { }
+  ngOnInit() {}
 
-  sideBarToggler() {
+  sideBarToggler(e) {
     this.sideBarOpen = !this.sideBarOpen;
   }
 }
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,16 +1,22 @@
-import { CommonModule } from '@angular/common';
-import { NgModule } from '@angular/core';
-import { FlexLayoutModule } from '@angular/flex-layout';
-import { MatButtonModule, MatDividerModule, MatIconModule, MatListModule, MatMenuModule, MatToolbarModule } from '@angular/material';
-import { RouterModule } from '@angular/router';
-import { HighchartsChartModule } from 'highcharts-angular';
-import { MapComponent } from '../modules/map/map.component';
-import { FooterComponent } from './components/footer/footer.component';
-import { HeaderComponent } from './components/header/header.component';
-import { SidebarComponent } from './components/sidebar/sidebar.component';
-import { AreaComponent } from './widgets/area/area.component';
-import { CardComponent } from './widgets/card/card.component';
-import { PieComponent } from './widgets/pie/pie.component';
+import { CommonModule } from "@angular/common";
+import { NgModule } from "@angular/core";
+import { FlexLayoutModule } from "@angular/flex-layout";
+import {
+  MatButtonModule,
+  MatDividerModule,
+  MatIconModule,
+  MatListModule,
+  MatMenuModule,
+  MatToolbarModule
+} from "@angular/material";
+import { RouterModule } from "@angular/router";
+import { HighchartsChartModule } from "highcharts-angular";
+import { FooterComponent } from "./components/footer/footer.component";
+import { HeaderComponent } from "./components/header/header.component";
+import { SidebarComponent } from "./components/sidebar/sidebar.component";
+import { AreaComponent } from "./widgets/area/area.component";
+import { CardComponent } from "./widgets/card/card.component";
+import { PieComponent } from "./widgets/pie/pie.component";
 
 @NgModule({
   declarations: [
@@ -19,8 +25,7 @@ import { PieComponent } from './widgets/pie/pie.component';
     SidebarComponent,
     AreaComponent,
     CardComponent,
-    PieComponent,
-    MapComponent
+    PieComponent
   ],
   imports: [
     CommonModule,
@@ -40,8 +45,7 @@ import { PieComponent } from './widgets/pie/pie.component';
     SidebarComponent,
     AreaComponent,
     CardComponent,
-    PieComponent,
-    MapComponent
+    PieComponent
   ]
 })
-export class SharedModule { }
+export class SharedModule {}
