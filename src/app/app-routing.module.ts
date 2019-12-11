8ecfc4f4e0fb46690af454a902a797ebcc113d50
edwd42 <edwd42@gmail.com>
2019-12-11 16:27:08 -0500

    fix paths in app-routing.module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,6 +6,7 @@ import { AboutComponent } from "./modules/about/about.component";
 import { DashboardComponent } from "./modules/dashboard/dashboard.component";
 import { PageNotFoundComponent } from "./modules/page-not-found/page-not-found.component";
 import { Plotly144Component } from "./shared/widgets/plotly144/plotly144.component";
+import { ProvStreetlightsComponent } from "./shared/widgets/prov-streetlights/prov-streetlights.component";
 import { OptimizationComponent } from "./tools/components/optimization/optimization.component";
 import { SimulationComponent } from "./tools/components/simulation/simulation.component";
 import { VisualizationComponent } from "./tools/components/visualization/visualization.component";
@@ -24,9 +25,10 @@ const routes: Routes = [
         component: AboutComponent
       },
       { path: "visualization", component: VisualizationComponent },
-      { path: "simulation", component: SimulationComponent },
-      { path: "optimization", component: OptimizationComponent },
+      { path: "prov-streetlights", component: ProvStreetlightsComponent },
       { path: "plotly144", component: Plotly144Component },
+      { path: "kwh-sim", component: SimulationComponent },
+      { path: "optimize-widget", component: OptimizationComponent },
 
       { path: "**", component: PageNotFoundComponent }
     ]
